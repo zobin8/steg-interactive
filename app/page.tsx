d7e48fafa8014d1bdbb8b42ed557b062f25de6be
zobin8 <28766833+zobin8@users.noreply.github.com>
2025-06-06 18:49:46 -0700

    Begin adding homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-      <main className="flex flex-col row-start-2 items-center sm:items-start">
-        Hello World!
-      </main>
+      <div className="flex grow bg-slate-200 justify-center">
+        <main className="flex bg-white shadow-md flex-col container text-center">
+          <h1 className="text-7xl font-extrabold p-7">Steg-Interactive</h1>
+          <h2 className="text-md">
+            A collection of interactive playgrounds to learn about steganography and cryptography techniques throughout history.
+          </h2>
+          <div className="p-4">
+          <Link
+            href="/playgrounds"
+            className="rounded-lg bg-purple-950 text-white font-bold p-3"
+          >
+            Get Started
+          </Link>
+          </div>
+        </main>
+      </div>
   );
 }
