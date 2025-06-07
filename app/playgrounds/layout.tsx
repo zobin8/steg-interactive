b3b705d17289af3632e583f00fa2636cce399215
zobin8 <28766833+zobin8@users.noreply.github.com>
2025-06-06 21:29:46 -0700

    Add collapsible sidebar on mobile

--- app/playgrounds/layout.tsx
+++ app/playgrounds/layout.tsx
@@ -1,13 +1,40 @@
+'use client';
+
 import SideNav from "@/app/ui/playgrounds/sidenav";
+import { Button, Drawer, DrawerHeader, DrawerItems } from 'flowbite-react';
+import { GoSidebarExpand } from "react-icons/go";
+import { useState } from 'react';
+
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleClose = () => setIsOpen(false);
+
   return (
     <div className="flex grow bg-slate-200 justify-center">
-      <div className="container flex-row flex shadow-md gap-8">
-        <div className="flex-none bg-white">
-          <SideNav />
+      <div className="container flex-row flex gap-8">
+        <div className="flex-none bg-white shadow-md hidden md:block">
+          <SideNav pad={true}/>
         </div>
-        <div className="flex-grow bg-white">{children}</div>
+        <div className="flex grow bg-white shadow-md">
+            <div className="block md:hidden justify-center p-2">
+              <Button
+                className="fixed"
+                onClick={() => setIsOpen(true)}
+                aria-label="Show playground navigation"
+              >
+                <GoSidebarExpand />
+              </Button>
+              <Drawer open={isOpen} onClose={handleClose}>
+                <DrawerHeader title="Playgrounds" titleIcon={() => <></>} />
+                <DrawerItems>
+                  <SideNav pad={false}/>
+                </DrawerItems>
+              </Drawer>
+            </div>
+            {children}
+        </div>
       </div>
     </div>
   );
--- app/ui/playgrounds/sidenav.tsx
+++ app/ui/playgrounds/sidenav.tsx
@@ -1,10 +1,10 @@
-"use client";
-
 import { Sidebar, SidebarItem, SidebarItemGroup, SidebarItems } from 'flowbite-react';
 
-export default function SideNav() {
+export default function SideNav({pad}: {pad: boolean}) {
+  const padClass = pad ? '' : '[&>div]:bg-transparent [&>div]:p-0';
+
   return (
-    <Sidebar aria-label="Playground navigation sidebar">
+    <Sidebar aria-label="Playground navigation sidebar" className={padClass}>
       <SidebarItems>
         <SidebarItemGroup>
           <SidebarItem href='/playgrounds'>Introduction</SidebarItem>
