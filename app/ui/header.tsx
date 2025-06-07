7448319bff842a2c97e7536b3a07a69b286cd0dc
zobin8 <28766833+zobin8@users.noreply.github.com>
2025-06-06 19:49:17 -0700

    Update header with flowbite

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -4,43 +4,47 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
+import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
+
+const links = [
+  {href: '/', name: 'Home'},
+  {href: '/playgrounds', name: 'Playgrounds'},
+]
 
 export default function Header() {
   const pathname = usePathname();
 
   function navText(href: string): string {
     return clsx(
-      'my-auto p-1 text-xl font-bold',
+      '',
       {
         'underline': pathname == href,
       },
     );
   };
 
-  function NavLink({href, name}: {href: string, name: string}) {
-    return (
-      <Link href={href} className={navText(href)}>{name}</Link>
-    );
-  }
-
   return (
-    <header className="bg-primary-950 text-white py-3 px-8">
-      <nav className="flex flex-row gap-8">
-        <Link href="/" className="flex flex-row">
+    <header className="dark shadow-md">
+      <Navbar>
+        <NavbarBrand as={Link} href="/">
           <Image
             src="/icon.png"
             width={256}
             height={256}
-            className="flex max-h-10 max-w-10"
+            className="flex max-h-10 max-w-10 mr-3"
             alt="Icon of steg-interactive, resembling a cipher wheel"
           ></Image>
-          <span className={navText('/')}>
-            Home
+          <span className="dark:text-white text-xl font-bold self-center">
+            Steg Interactive
           </span>
-        </Link>
-        <NavLink href="/playgrounds" name="Playgrounds">
-        </NavLink>
-      </nav>
+        </NavbarBrand>
+        <NavbarToggle />
+        <NavbarCollapse>
+          {links.map((link) => (
+            <NavbarLink key={link.href} as={Link} className={navText(link.href)} href={link.href}>{link.name}</NavbarLink>
+          ))}
+        </NavbarCollapse>
+      </Navbar>
     </header>
   );
 }
