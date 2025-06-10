2bb7a79de0c6449deb3e1cf3bdb119081b81dea6
zobin8 <28766833+zobin8@users.noreply.github.com>
2025-06-09 19:19:28 -0700

    Fix missing information in introduction

--- app/playgrounds/(introduction)/page.tsx
+++ app/playgrounds/(introduction)/page.tsx
@@ -3,6 +3,7 @@ export default function Component() {
     <div className="flex flex-col gap-3">
       <h1 className="text-2xl">Introduction</h1>
       <h2 className="text-xl">Under Construction</h2>
+      <p>This page is still under construction. Check back later!</p>
     </div>
   );
 }
