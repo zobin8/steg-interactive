de6487b31b93f6a69341a585761b42972bf8264a
zobin8 <28766833+zobin8@users.noreply.github.com>
2025-06-19 17:44:02 -0700

    Add basic caeser ciphertable

--- app/playgrounds/ancient/caeser/page.tsx
+++ app/playgrounds/ancient/caeser/page.tsx
@@ -1,10 +1,41 @@
+'use client'
+
 import { Footnote, FootnoteList, FootnoteProvider } from "@/app/ui/playgrounds/footnote";
 import Heading from "@/app/ui/playgrounds/heading";
 
-import { Alert } from "flowbite-react";
+import { Alert, RangeSlider, Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from "flowbite-react";
 import Link from "next/link";
+import { useState } from "react";
+import { FaLongArrowAltRight } from "react-icons/fa";
+
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+function shiftText(text: string, shift: number): string {
+  text = text.toUpperCase();
+  var output = '';
+
+  const start = 'A'.charCodeAt(0);
+  for (var ch of text) {
+    if (ch > 'Z' || ch < 'A') {
+      output += ch;
+    } else {
+      var index = ch.charCodeAt(0) - start;
+      var shiftIndex = (index + shift) % 26;
+      output += String.fromCharCode(shiftIndex + start);
+    }
+  }
+
+  return output;
+}
 
 export default function Component() {
+  const [key, setKey] = useState(1);
+
+  var alphabetParts = [];
+  for (var start = 0; start < alphabet.length; start += 13) {
+    alphabetParts.push(alphabet.slice(start, start + 13));
+  }
+
   return (
     <FootnoteProvider>
       <div className="flex flex-col gap-3">
@@ -22,10 +53,35 @@ export default function Component() {
             See <Link className="text-primary-800 hover:text-primary-700" href="https://www.britannica.com/topic/Caesar-cipher">https://www.britannica.com/topic/Caesar-cipher</Link>
           </Footnote>
         </p>
-        <Heading level={2} name="Construction" />
+        <Heading level={2} name="Example" />
         <p>
-
+          Let's try an example. Pick a key by using the slider below, and watch the cipher table change.
         </p>
+        <p>
+          Key: A <FaLongArrowAltRight className="inline" /> {shiftText('A', key)}
+        </p>
+        <RangeSlider id="key1" sizing="lg" min={1} max={25} value={key} onChange={(evt) => setKey(Number(evt.target.value))} />
+        <p>Cipher table:</p>
+        <Table>
+          {alphabetParts.map((plaintext) => (
+            <>
+              <TableHead>
+                <TableRow>
+                  {plaintext.map((ch) => (
+                    <TableHeadCell key={ch}>{ch}</TableHeadCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody className="divide-y">
+                <TableRow>
+                  {plaintext.map((ch) => (
+                    <TableCell key={ch}>{shiftText(ch, key)}</TableCell>
+                  ))}
+                </TableRow>
+              </TableBody>
+            </>
+          ))}
+        </Table>
         <Heading level={2} name="Security" />
         <p>
 
