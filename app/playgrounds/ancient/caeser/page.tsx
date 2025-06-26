c3f4c9256033e7941845d2833e1d23f88510967c
zobin8 <28766833+zobin8@users.noreply.github.com>
2025-06-25 22:13:02 -0700

    Add TryItOut widget

--- app/playgrounds/ancient/caeser/page.tsx
+++ app/playgrounds/ancient/caeser/page.tsx
@@ -4,9 +4,9 @@ import CipherTable from "@/app/ui/playgrounds/ciphertable";
 import { Footnote, FootnoteList, FootnoteProvider } from "@/app/ui/playgrounds/footnote";
 import Heading from "@/app/ui/playgrounds/heading";
 
-import { RangeSlider } from "flowbite-react";
+import { Label, RangeSlider, Textarea } from "flowbite-react";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { FaLongArrowAltRight } from "react-icons/fa";
 
 const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -31,9 +31,43 @@ function shiftText(text: string, shift: number): string {
   return output;
 }
 
-export default function Component() {
-  const [key1, setKey1] = useState(1);
-  const [key2, setKey2] = useState(1);
+function KeySlider({value, setKey, id, onChange = undefined}: any) {
+  return (
+    <>
+      <p>
+        Key: A <FaLongArrowAltRight className="inline" /> {shiftText('A', value)}
+      </p>
+      <RangeSlider
+        id={id}
+        sizing="lg"
+        min={1}
+        max={25}
+        value={value}
+        onChange={(evt) => {
+          setKey(Number(evt.target.value));
+          onChange(evt);
+        }}
+      />
+    </>
+  );
+}
+
+function ExampleTable() {
+  const [key, setKey] = useState(1);
+
+  return (
+    <>
+      <KeySlider value={key} setKey={setKey} id="key1"/>
+      <CipherTable
+        plaintext={alphabet}
+        ciphertext={shiftText(alphabet, key)}
+      />
+    </>
+  );
+}
+
+function DecodingChallenge() {
+  const [key, setKey] = useState(1);
   const [secretKey, setSecret] = useState(2);
 
   // Set secret key on page load
@@ -43,6 +77,75 @@ export default function Component() {
 
   const plaintext = shiftText(secret, -secretOffset);
   const shiftedSecret = shiftText(plaintext, secretKey);
+
+  return (
+    <>
+      <KeySlider value={key} setKey={setKey} id="key2"/>
+      <CipherTable
+        plaintext={shiftText(shiftedSecret, -key)}
+        ciphertext={shiftedSecret}
+        reverse={true}
+      />
+    </>
+  );
+}
+
+function TryItOut() {
+  const [key, setKey] = useState(1);
+  const [plaintext, setPlaintext] = useState('');
+  const [ciphertext, setCiphertext] = useState('');
+  const [lastAction, setLastAction] = useState('encode');
+
+  function createHandler(action: 'encode'|'decode'|'update') {
+    function handleChange(event: ChangeEvent) {
+      var text = event.target.value;
+      var currentKey = key;
+      if (action == 'update') {
+        action = lastAction;
+        currentKey = Number(event.target.value);
+        text = lastAction == 'encode' ? plaintext : ciphertext;
+      }
+
+      if (action == 'encode') {
+        setLastAction(action);
+        setPlaintext(text);
+        setCiphertext(shiftText(text, currentKey));
+      } else if (action == 'decode') {
+        setLastAction(action);
+        setCiphertext(text);
+        setPlaintext(shiftText(text, -currentKey));
+      }
+    }
+
+    return handleChange
+  }
+
+  return (
+    <>
+      <KeySlider value={key} setKey={setKey} id="key3" onChange={createHandler('update')}/>
+      <Label htmlFor="plaintext">Plaintext:</Label>
+      <Textarea
+        id="plaintext"
+        placeholder="Type here..."
+        rows={3}
+        value={plaintext}
+        onChange={createHandler('encode')}
+      />
+      <Label htmlFor="ciphertext">Ciphertext:</Label>
+      <Textarea
+        id="ciphertext"
+        placeholder="Type here..."
+        rows={3}
+        value={ciphertext}
+        onChange={createHandler('decode')}
+      />
+      <FootnoteList></FootnoteList>
+    </>
+  );
+}
+
+export default function Component() {
+  const [key3, setKey3] = useState(1);
 
   return (
     <FootnoteProvider>
@@ -57,34 +160,23 @@ export default function Component() {
             See <Link className="text-primary-800 hover:text-primary-700" href="https://www.britannica.com/topic/Caesar-cipher">https://www.britannica.com/topic/Caesar-cipher</Link>
           </Footnote>
         </p>
+
         <Heading level={2} name="Example" />
         <p>
           Let's try an example. Pick a key by using the slider below, and watch the cipher table change.
         </p>
-        <p>
-          Key: A <FaLongArrowAltRight className="inline" /> {shiftText('A', key1)}
-        </p>
-        <RangeSlider id="key1" sizing="lg" min={1} max={25} value={key1} onChange={(evt) => setKey1(Number(evt.target.value))} />
-        <CipherTable
-          plaintext={alphabet}
-          ciphertext={shiftText(alphabet, key1)}
-        />
+        <ExampleTable/>
+
         <Heading level={2} name="Security" />
         <p>
           Since there are only 25 possible keys, it is rather easy to try all combinations until the text is readable.
           Additionally, the ciphertext is very obviously encrypted, so there is little steganographic security.
           Try it below:
         </p>
-        <p>
-          Key: A <FaLongArrowAltRight className="inline" /> {shiftText('A', key2)}
-        </p>
-        <RangeSlider id="key2" sizing="lg" min={1} max={25} value={key2} onChange={(evt) => setKey2(Number(evt.target.value))} />
-        <CipherTable
-          plaintext={shiftText(shiftedSecret, -key2)}
-          ciphertext={shiftedSecret}
-          reverse={true}
-        />
-        <FootnoteList></FootnoteList>
+        <DecodingChallenge />
+
+        <Heading level={2} name="Try it out" />
+        <TryItOut />
       </div>
     </FootnoteProvider>
   );
