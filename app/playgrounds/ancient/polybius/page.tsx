babd4ae5376a49bf4e36d4607f9118302eeac4f0
zobin8 <28766833+zobin8@users.noreply.github.com>
2025-07-01 21:38:20 -0700

    Add start of Polybius page

---
+++ app/playgrounds/ancient/polybius/page.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import { Footnote, FootnoteList, FootnoteProvider } from "@/app/ui/playgrounds/footnote";
+import Heading from "@/app/ui/playgrounds/heading";
+import { Alert } from "flowbite-react";
+
+import Link from "next/link";
+
+// Methods
+
+// Subcomponents
+
+// Export
+
+export default function Component() {
+  return (
+    <FootnoteProvider>
+      <div className="flex flex-col gap-3">
+        <Alert color="warning">
+          <span className="font-medium me-1">Under Construction!</span>
+          Parts of this page are unfinished. Sections may be missing or incomplete.
+        </Alert>
+
+        <Heading level={1} name="Polybius Square" />
+        <p>
+          The Polybius Square, popularized by the ancient greek historian Polybius, is a table used for substitution ciphers.
+          Its original use was to reduce the alphabet into the numbers 1 through 5, for easier use in fire signaling.
+          However, this ability to fractionalize the alphabet into smaller symbols was used throughout an entire family of ciphers.
+          <Footnote>
+            The paper
+            <Link className="text-primary-800 hover:text-primary-700 mx-1" href="https://www.researchgate.net/publication/342637897_Polybius_Square_in_Cryptography_A_Brief_Review_of_Literature">"Polybius Square in Cryptography: A Brief Review of Literature"</Link>
+            covers this in depth.
+          </Footnote>
+        </p>
+
+        <Heading level={2} name="Original Design" />
+        <p>
+
+        </p>
+
+        <Heading level={2} name="Use in Steganography" />
+        <p>
+          
+        </p>
+
+        <Heading level={2} name="Variation with Key" />
+        <p>
+          
+        </p>
+
+        <Heading level={2} name="Try it Out" />
+        <p>
+          
+        </p>
+
+        <FootnoteList></FootnoteList>
+      </div>
+    </FootnoteProvider>
+  );
+}
